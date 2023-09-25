5a455746611067db3d368c3d2a604ddc4e81bad6
AnusthaM <anusthamaharjan72@gmail.com>
2023-09-25 16:31:53 +0545

    js1

--- index.js
+++ index.js
@@ -20,3 +20,22 @@ console.log(da);
 
 const isValid = true;
 console.log(isValid);
+
+alert("anustha");
+
+let personName=prompt("what is your name?");
+console.log(personName);
+
+const isAbove18=confirm("Are you above 18?");
+console.log(isAbove18);
+
+const isOfAge =isAbove18? "above":"below";
+const text=
+"My name is "+ personName + 
+" and I am " + isOfAge + " 18 years";
+
+alert(text);
+
+// Write a js code that asks for username and password with confirm password,
+//if the username and password matches, alert "You have sucessfully logged in"
+//else alert "Username and password is incorrect"
