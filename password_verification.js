1f6b893548e70a71a56edbcea9a92921185c09dd
AnusthaM <anusthamaharjan72@gmail.com>
2023-09-25 21:43:47 +0545

    assign1

---
+++ password_verification.js
@@ -0,0 +1,19 @@
+let username="anustha";
+const password="12345";
+
+const user=prompt("Username =");
+console.log(user);
+
+let pass=prompt( "Password =");
+console.log(pass);
+
+
+
+if((user==username) && (pass==password)){
+
+ alert("You have sucessfully logged in");
+
+}else{
+    alert("Username and password is incorrect");
+}
+
